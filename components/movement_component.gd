a84e840dec324fdbb3330d9a1ad4bbd6f144791f
Thelmo Martins <thelmo@gmail.com>
2025-04-08 13:49:15 -0300

    Advanced movement implemented.
    Following the part 2 of the tutorial: https://www.youtube.com/watch?v=3sWH2lsntek

--- components/movement_component.gd
+++ components/movement_component.gd
@@ -4,6 +4,17 @@ extends Node
 
 @export_subgroup("Settings")
 @export var speed: float = 200.0
+@export var ground_acceleration: float = 15.0
+@export var ground_deceleration: float = 15.0
+@export var air_acceleration: float = 25.0
+@export var air_deceleration: float = 5.0
+
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
-    body.velocity.x = direction * speed
+    var acceleration: float = 0.0
+    if body.is_on_floor():
+        acceleration = ground_acceleration if direction !=0 else ground_deceleration
+    else:
+        acceleration = air_acceleration if direction !=0 else air_deceleration
+    
+    body.velocity.x = move_toward(body.velocity.x, direction * speed, acceleration)
